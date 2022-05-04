a10501b9b881fe0f34af9f7c20587f9106f6c793
Aqua Minato <sweet4182691@gmail.com>
2022-05-04 16:47:06 +0900

    2 end

--- tour2/src/main.rs
+++ tour2/src/main.rs
@@ -1,4 +1,5 @@
 fn if_branch() {
+    // この書き方だとmatch文使おうってwarning出される
     let x = 10;
     if x < 10 {
         println!("xは10より小さい");
@@ -41,13 +42,74 @@ fn for_loop() {
     }
 }
 
+fn match_1() {
+    let x = 5;
+    match x {
+        1 => println!("xは1です"),
+        2 => println!("xは2です"),
+        3 => println!("xは3です"),
+        4 => println!("xは4です"),
+        5 => println!("xは5です"),
+        _ => println!("xは1以上5以下ではありません"), // これ書かないとエラーになる
+    }
+}
+
+fn match_2() {
+    let x = 50;
+    match x {
+        1..=10 => {
+            println!("1から10までの中に{}はあります", x);
+        }
+        11..=20 => {
+            println!("11から20までの中に{}はあります", x);
+        }
+        matched_num @ 21..=100 => {
+            println!("found {} number between 10 to 100!", matched_num);
+        }
+        _ => {
+            println!("xは1以上100以下ではありません");
+        }
+    }
+}
+
+fn variable_loop() {
+    let mut x = 0;
+    let v = loop {
+        x += 1;
+        if x == 10 {
+            break "10なう";
+        }
+    };
+    println!("{}", v); // 10なう が入る
+}
+
+fn ternary() -> i32 {
+    let x = 42;
+
+    // 三項式
+    let v = if x < 42 { -1 } else { 1 };
+
+    v
+}
+
+fn ex1() -> i32 {
+    let v = {
+        let x = 1;
+        let y = 2;
+        x + y
+    };
+    v + 10 // return
+}
+
 fn main() {
     if_branch();
-    println!("############################");
     loop_();
-    println!("############################");
     while_();
-    println!("############################");
     for_loop();
+    match_1();
+    match_2();
+    variable_loop();
+    println!("{}", ternary());
     println!("############################");
+    println!("{}", ex1());
 }
