3cb62483d1e5deab008c42ce6b719a8e8f5977a3
Aqua Minato <sweet4182691@gmail.com>
2022-05-05 13:24:00 +0900

    block test

--- sandbox1/src/main.rs
+++ sandbox1/src/main.rs
@@ -1,11 +1,29 @@
-fn main() {
+fn _vector_test() {
     let animal_box = vec!["cat", "dog", "bird"];
 
-    for i in 0..animal_box.len() {
-        println!("{}", animal_box[i]);
-    }
+    // warn
+    // for i in 0..animal_box.len() {
+    //     println!("{}", animal_box[i]);
+    // }
 
     for i in animal_box {
         println!("{}", i);
     }
+
+    let v = vec![1, 2, 3, 4, 5, 6];
+    let odd_numbers = v.iter().filter(|&x| x % 2 != 0); // イテレータ
+    print!("{:?}", odd_numbers);
+}
+
+fn test() {
+    let a;
+    {
+        a = 5;
+        println!("a = {}", a);
+    }
+    // println!(a)  // error
+}
+
+fn main() {
+    test();
 }
