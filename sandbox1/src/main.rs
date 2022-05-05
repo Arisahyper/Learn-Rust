3145a2dc3ea3fdccd710ee3b76febdbddc233c57
Aqua Minato <sweet4182691@gmail.com>
2022-05-05 12:28:40 +0900

    vector

--- sandbox1/src/main.rs
+++ sandbox1/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let animal_box = vec!["cat", "dog", "bird"];
+
+    for i in 0..animal_box.len() {
+        println!("{}", animal_box[i]);
+    }
+
+    for i in animal_box {
+        println!("{}", i);
+    }
 }
